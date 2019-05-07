8a12f0aa18ebddfed29ae230f69ad0aa2f86752a
13331033844 <49219593+13331033844@users.noreply.github.com>
2019-05-07 13:22:02 +0800

    防连击2

--- lock.js
+++ lock.js
@@ -1,23 +1,39 @@
-	A=0;
+	Public_lock=0;
+	minitime=500
 	function Lock(B) {
-		if (A==0) {
-			A=1;
+		if (Public_lock==0) {
+			Public_lock=1;
 			window.setTimeout(function () {
-				A=0;
-			},500); 
+				Public_lock=0;
+			},minitime); 
 			return B();
 		}else{
 			return false
 		}
 	}
 
+	function aclick(obj) {
+		if (obj.attr('href')==undefined) {
+			return false
+		}else{
+			href=obj.attr('href')
+			obj.removeAttr('href')
+			window.setTimeout(function () {
+			obj.attr('href',href);
+			},minitime); 
+		}
+	}
+
+
+	// $('a').click(function () {
+	// 	aclick($(this))
+	// })
+
+
 
 
 	// $('.btn').click(function () {
 	// 	Lock(function () {
-
 	// 		console.log(1)
-
 	// 	})
-		
 	// })
